28cfd672ced876230b8955e9fcc4546d4d9c2342
Mack <macknz7@gmail.com>
2020-12-01 19:01:01 -0800

    User Profile - Edit Avatar

---
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RockTransactions.Data;
+using RockTransactions.Extensions;
+using RockTransactions.Models;
+using RockTransactions.Services;
+
+namespace RockTransactions.Areas.Identity.Pages.Account.Manage
+{
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<FPUser> _userManager;
+        private readonly SignInManager<FPUser> _signInManager;
+        private readonly IFPFileService _fileService;
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(
+            UserManager<FPUser> userManager,
+            SignInManager<FPUser> signInManager,
+            IFPFileService fileService,
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _fileService = fileService;
+            _context = context;
+        }
+
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            [Display(Name = "Avatar")]
+            [NotMapped]
+            [DataType(DataType.Upload)]
+            [MaxFileSize(2 * 1024 * 1024)]
+            [AllowedExtensions(new string[] { ".jpg", ".png" })]
+            public IFormFile Avatar { get; set; }
+        }
+
+        private async Task LoadAsync(FPUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+
+            Input = new InputModel
+            {
+                PhoneNumber = phoneNumber
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    StatusMessage = "Unexpected error when trying to set phone number.";
+                    return RedirectToPage();
+                }
+            }
+
+            var bytes = await _fileService.ConvertFileToByteArrayAsync(Input.Avatar);
+            var inputFile = _fileService.ConvertByteArrayToFile(bytes, Input.Avatar.FileName.Split('.')[1]);
+            var existingFile = _fileService.ConvertByteArrayToFile(user.FileData, user.FileName.Split('.')[1]);
+            if (inputFile != existingFile)
+            {
+                user.FileData = bytes;
+                user.FileName = Input.Avatar.FileName;
+                await _context.SaveChangesAsync();
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
+        }
+    }
+}
