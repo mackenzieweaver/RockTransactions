a828b0d7412d0020699e5ffc81e43ae87e5f76ea
Mack <macknz7@gmail.com>
2020-12-06 18:07:49 -0800

    Modal hides then sweet alert

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,6 @@
 ﻿$(document).on('click', '#hhDashInviteSubmit', function InviteAlert(e) {
     e.preventDefault();
+    $("#exampleModal").modal("hide");
     swal("Good news!", "Your invitation has been sent.", "success")
         .then(() => $('#dashInviteForm').submit());
 });
